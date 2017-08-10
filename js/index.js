dde943d5b3091098feee8530e55a55447256426c
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-08-10 16:58:52 +0530

    tooltip made

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	$("#myRange").change(function(){
-		var x = document.getElementById("myRange").value;
-		console.log(x);
-	})
-	$('.project-card').click(function(){
-		$(this).addClass('card-animation');
-		$('.content-holder').fadeOut(500);
-		$('.info').delay(2000).fadeIn(200);
-	})
+
 
 })
